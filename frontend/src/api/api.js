110758f05fe28d9ca3f887b1da6c7d0067ab7fb2
Leena2807 <ghuge.leena.vijay28@gmail.com>
2026-01-14 19:12:32 +0530

    Update api.js

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: "https://gigflow-backend-uyin.onrender.com",
-  withCredentials: true
+  withCredentials: true,
 });
 
 export default api;
